44f6971d9c9ea3a69d729d5413a4ed472b3a7fd0
JovialCoder <mike+github@jovialcoder.com>
2019-10-21 22:57:51 -0400

    Initial update changes

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>CI-CD Pipeline Demo</title>
+  <style>
+    body {
+        background-color: #182B37;
+        color: #FFF;
+    }
+    .headerlogo {
+        float: left;
+        margin-top: 20px;
+        margin-left: 20px;
+    }
+    .welcometext {
+        float: left; 
+        clear: both;
+        margin-left: 50px;
+        margin-top: 20px;
+    }
+  </style>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+    <img id="headerlogo" src="https://linuxacademy.com/templates/cp/assets/img/la-logo-white.png" />
+    <?php echo'<h1 id="welcometext">Hello World</h1>' ?>
+    
  </body>
 </html>
+
